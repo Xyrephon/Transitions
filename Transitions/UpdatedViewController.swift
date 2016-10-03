c88e6eef011cdb5bbe3b03d92ceccfce501ec692
Xyrephon <jtuc1613@740s-pl-115.local>
2016-10-03 08:00:12 -0600

    Changed color of background and added another swift view.

---
+++ Transitions/UpdatedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpdatedViewController.swift
+//  Transitions
+//
+//  Created by Tucker, Jonah on 10/3/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    override func viewDidLoad()
+    {
+        //Load the parent version of a View.
+        super.viewDidLoad()
+        //Load this pages components.
+    }
+}
